b3c0b750ce245c39525573eac57e1094c6e286e7
0la <olga.lsk@gmail.com>
2016-07-30 20:51:32 +0300

    edited code style

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 //1
 function pow(n1, n2) {
-var b;
-if (n2 < 0) {
-	b = n1;
-	for (var i = 1; i < (n2)  *(-1); i++) {
-	b *= n1;
+	var b;
+	if (n2 < 0) {
+		b = n1;
+		for (var i = 1; i < (n2)  *(-1); i++) {
+		b *= n1;
+		}
+		return 1 / b;
 	}
-	return 1  /b;
-}
-if (n2 === 0)
-		return 1;
-	if (n2 % 2 == 1)
-		return pow (n1, n2 - 1) * n1;
+	if (n2 === 0) return 1;
+	if (n2 % 2 == 1) return pow (n1, n2 - 1) * n1;
 	else {
 		b = pow (n1, n2 / 2);
 		return b * b;
@@ -22,7 +20,7 @@ function isInteger(num) {
 	else return false;
 }
 function inputNum(string) {
-	var count=0;var s='';
+	var count = 0;var s = '';
 	do {
 		if ( count > 0 ) s = 'Input integer number, please.';
 		++count;
@@ -36,18 +34,18 @@ console.log( pow(x, n) );
 
 //2
 var arrNames=[];
-for (var i=0;i<5;i++) {
-	arrNames[i]=prompt("Введите "+(i+1)+" имя:");
+for (var i = 0; i < 5; i++) {
+	arrNames[i]=prompt("Введите " + (i+1) + " имя:");
 }
 var userName = prompt ("Введите имя пользователя:");
-var chars=0; var isUser=false;
-for (var i=0; i<arrNames.length; i++) {
+var chars = 0; var isUser = false;
+for (var i = 0; i < arrNames.length; i++) {
 	if (arrNames[i].length === userName.length)
-		for (var j=0;j<arrNames[i].length;j++) {
-			if (arrNames[i][j]===userName[j]) ++chars;
+		for (var j = 0; j < arrNames[i].length; j++) {
+			if (arrNames[i][j] === userName[j]) ++chars;
 		}
-	if (arrNames[i].length===chars&&chars>0) isUser=true;
-	else chars=0;
+	if (arrNames[i].length === chars && chars > 0) isUser = true;
+	else chars = 0;
 }
-if (isUser) alert(userName+", вы успешно вошли");
+if (isUser) alert(userName + ", вы успешно вошли");
 else alert("нет совпадения");
